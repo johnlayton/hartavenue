0bfd24be2c04f9acbb7c2ae8577b7b6016837c10
john layton <john.layton@touchcorp.com>
2017-10-01 09:45:10 +1100

    add julinda court application

---
+++ Casks/julindacourt.rb
@@ -0,0 +1,10 @@
+cask 'julindacourt' do
+  version '0.0.0'
+  sha256 'cdd9ae68bcb94a157e2512163b172983f83f1db127a5f42e9482983cbe886123'
+
+  url 'https://github.com/johnlayton/julindacourt/releases/download/v0.0.0/julindacourt-0.0.0-mac.zip'
+  name 'julindacourt'
+  homepage ''
+
+  app 'julindacourt.app'
+end
