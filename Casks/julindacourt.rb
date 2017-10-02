e7df461599810931757ac86ea0f96c4eb3f98e44
john layton <john.layton@touchcorp.com>
2017-10-02 21:54:35 +1100

    update julinda court application version

--- Casks/julindacourt.rb
+++ Casks/julindacourt.rb
@@ -1,5 +1,5 @@
 cask 'julindacourt' do
-  version '0.0.0'
+  version '0.0.1'
   sha256 'b2fd2d6bcc5b739e8cbc355d6dad0393dbb8a63fafe0624d8cd8b6f1b4a36f0d'
 
   url 'https://github.com/johnlayton/julindacourt/releases/download/v0.0.1/julindacourt-0.0.1-mac.zip'
