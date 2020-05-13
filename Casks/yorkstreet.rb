052d818f18faebe65237473812af88d9a3ca3a92
John Layton <john.layton@mebank.com.au>
2020-05-13 20:21:55 +1000

    add yorke street

---
+++ Casks/yorkstreet.rb
@@ -0,0 +1,10 @@
+cask 'yorkstreet' do
+  version '0.0.1'
+  sha256 'b2fd2d6bcc5b739e8cbc355d6dad0393dbb8a63fafe0624d8cd8b6f1b4a36f0d'
+
+  url 'https://github.com/johnlayton/yorkstreet/releases/download/v0.0.1/yorkstreet-0.0.1-mac.zip'
+  name 'julindacourt'
+  homepage 'https://github.com/johnlayton/yorkstreet'
+
+  app 'julindacourt.app'
+end
