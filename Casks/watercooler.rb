0f8c83e5916f81eb1242e3fae1e0a60acf25c06e
John Layton <john.layton@mebank.com.au>
2020-07-29 17:17:35 +1000

    Add watercooler app

---
+++ Casks/watercooler.rb
@@ -0,0 +1,10 @@
+cask 'watercooler' do
+  version '0.0.1'
+  sha256 '9c0bebd6abb926756bd7b594c7b34f7b6555f743c469f1f82e155dd34f500a7e'
+
+  url 'https://water-cooler.s3-us-west-1.amazonaws.com/watercooler.dmg'
+  name 'watercooler'
+  homepage 'https://waterc00ler.com/'
+
+  app 'Watercooler.app'
+end
